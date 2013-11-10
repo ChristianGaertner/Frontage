80360baff09759b233e28919bceb2a9e78b8cf1a
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-11-10 17:10:14 +0100

    Created base Frontage class

---
+++ src/DaGardner/Frontage/Frontage.php
@@ -0,0 +1,37 @@
+<?php namespace DaGardner\Frontage;
+/*
+ *  (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the Frontage package
+ * License: View distributed LICENSE file
+ */
+
+/**
+ * Frontage's main class
+ * @author Christian Gärtner <christiangaertner.film@googlemail.com>
+ */
+abstract class Frontage
+{
+	/**
+	 * The container for this class to look for objects
+	 * @var \DaGardner\DaContainer\ResolverInterface
+	 */
+	protected static $container;
+
+	/**
+	 * Inject the container.
+	 * @param \DaGardner\DaContainer\ResolverInterface $container The container
+	 */
+	public static function setContainer(\DaGardner\DaContainer\ResolverInterface $container)
+	{
+		static::$container = $container;
+	}
+
+	abstract public static function getFacadeID();
+
+	public static function __callStatic($method, $params)
+	{
+		$instance = static::$container->resolve(static::getFacadeID());
+
+		return call_user_func_array(array($instance, $method), $params);
+	}
+}
